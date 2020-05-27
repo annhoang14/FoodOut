122264d1b27f236c3ad4ed3983e469825cd47f88
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-05-27 09:59:09 -0400

    map display functional

--- src/map-display/MapDisplay.js
+++ src/map-display/MapDisplay.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React from 'react';
 
-const MapDisplay = () => {
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-  return (
-    <div>
-      
-    </div>
-  )
+import './MapDisplay.css'
+
+const MapDisplay = ({ searchLocation }) => {
+  React.useEffect(() => {
+    // create map
+    const mapObj = L.map('mapid', {
+      center: [38.033554, -78.507980],
+      zoom: 16,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
+  }, []);
+
+  return <div id="mapid"></div>
 }
 
 export default MapDisplay;
